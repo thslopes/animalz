a08f2951822a9045e865f2d98beb80637517f6b1
Thiago Lopes <thiago.slopes@b2wdigital.com>
2021-11-20 18:47:09 -0300

    show images

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+var pic = require('./assets/pics/e1.jpeg');
+var pic2 = require('./assets/pics/d1.jpg');
+var pic3 = require('./assets/pics/t1.jpg');
+
+var pics = [pic, pic2, pic3]
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { imgIdx: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(prevState => ({
+      imgIdx: (prevState.imgIdx +1)%3
+    }));
+  }
+  render() {
+    return (
+      <View >
+        <StatusBar style="auto" />
+        <TouchableOpacity onPress={this.handleClick} >
+        <Image 
+          source={pics[this.state.imgIdx]}
+          style={{ width: 375, height: 667 }}
+        />
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
